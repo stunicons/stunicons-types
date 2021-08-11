4952b52d9c972fa8099ec0f407621c51f7aaa9ab
Ntwari Clarance Liberiste <liberintwari@gmail.com>
2021-08-11 15:07:39 +0200

    stunicons type

---
+++ index.ts
@@ -0,0 +1,104 @@
+export type stunicon = "si-home" |
+"si-menu-bar" |
+"si-plus" |
+"si-search" |
+"si-user" |
+"si-arrow-down" |
+"si-arrow-left" |
+"si-arrow-right" |
+"si-arrow-up" |
+"si-copyright" |
+"si-dashboard" |
+"si-delete" |
+"arrow up" |
+"si-document" |
+"si-download" |
+"si-exit" |
+"si-eye" |
+"si-gmail" |
+"si-heart" |
+"si-location-off" |
+"si-location-on" |
+"si-facebook" |
+"si-instagram" |
+"si-linkedin" |
+"si-youtube" |
+"si-more-horizontal" |
+"si-more-vertical" |
+"si-movie" |
+"si-share" |
+"si-thumbs-down" |
+"si-thumbs-up" |
+"si-video" |
+"si-bookmark" |
+"si-chevron-left" |
+"si-chevron-right" |
+"si-chevron-down" |
+"si-chevron-up" |
+"si-dribbble" |
+"si-edit" |
+"si-file-list" |
+"si-folder" |
+"si-star" |
+"si-upload" |
+"si-mic" |
+"si-moon" |
+"si-activity" |
+"si-alert" |
+"si-align-justify" |
+"si-align-right" |
+"si-align-left" |
+"si-apps" |
+"si-archive" |
+"si-award" |
+"si-briefcase" |
+"si-calendar" |
+"si-camera" |
+"si-chart-1" |
+"si-chat-history" |
+"si-close" |
+"si-cloud" |
+"si-contrast" |
+"si-dollar-sign" |
+"si-error" |
+"si-exit-full-screen" |
+"si-flag" |
+"si-flash" |
+"si-full-screen" |
+"si-globe" |
+"si-honour" |
+"si-image" |
+"si-info" |
+"si-label" |
+"si-layers" |
+"si-link" |
+"si-lock"|
+"si-menu-alt"|
+"si-message"|
+"si-open-door"|
+"si-paper-clip"|
+"si-pause"|
+"si-phone-2"|
+"si-pie-chart-2"|
+"si-play-1"|
+"si-play-2"|
+"si-printer"|
+"si-pushpin"|
+"si-reply"|
+"si-restart"|
+"si-send-2"|
+"si-shopping-bag"|
+"si-shopping-cart"|
+"si-speaker"|
+"si-tag"|
+"si-time"|
+"si-users"|
+"si-window"|
+"si-zoom-in"|
+"si-zoom-out"|
+"si-github"|
+"si-slack"|
+"si-twitter"|
+"si-settings"|
+"si-behance"|
+"si-phone"
